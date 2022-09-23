82a099a52bc8bff2a9ebdb806d35a57f84abe716
frappe <frappe@ubuntu.vm>
2022-09-23 02:21:56 +0000

    App Structure Planing

---
+++ devops/devops/doctype/scrum_product_backlog_item/test_scrum_product_backlog_item.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, BakrAldubai and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestScrumProductBacklogItem(unittest.TestCase):
+	pass
---
+++ devops/modules/doctype/functions_sequense/test_functions_sequense.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, dgk and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestFunctionsSequense(unittest.TestCase):
+	pass
